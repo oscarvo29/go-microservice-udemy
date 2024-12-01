42cc784c8c0053ac796aaa6af0b0968bb63e8f6c
oscarvo29 <oscar.vinther@gmail.com>
2024-12-01 17:10:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+# Working with Microservices in Go (Golang)
+
+A project buildt while following the Udemy course **"[Working with Microservices in Go (Golang)]"** by [Trevor Sawler] (https://www.udemy.com/user/trevor-sawler/?kw=Trevor+Sawler&src=sac)
+
+This project was developed as part of my learning journey through the Udemy course **"[Working with Microservices in Go (Golang)]"**. This course focuses on build scalable micro services in go.
---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "80"
+
+var counts int64
+
+type Config struct {
+	Repo   data.Respository
+	Client *http.Client
+}
+
+func main() {
+	log.Println("Starting authentication service")
+
+	// connect to db
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgress")
+	}
+
+	// set up config
+	app := Config{
+		Client: &http.Client{},
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgress not yet ready..")
+			counts++
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("back off for two seconds")
+		time.Sleep(2 * time.Second)
+		continue
+	}
+}
+
+func (app *Config) setupRepo(conn *sql.DB) {
+	db := data.NewPostRepository(conn)
+	app.Repo = db
+}
